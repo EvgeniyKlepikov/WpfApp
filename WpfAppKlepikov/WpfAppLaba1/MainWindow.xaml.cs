67aea8e0258676d1a0495189d3c731a1c13f6804
EvgeniyKlepikov <e_klepikov@mail.ru>
2024-12-13 00:14:46 +0300

    Лабораторная работа 1.

--- WpfAppKlepikov/WpfAppLaba1/MainWindow.xaml.cs
+++ WpfAppKlepikov/WpfAppLaba1/MainWindow.xaml.cs
@@ -16,6 +16,8 @@ namespace WpfAppLaba1
     /// </summary>
     public partial class MainWindow : Window
     {
+        double x;
+        char operation;
         public MainWindow()
         {
             InitializeComponent();
@@ -32,7 +34,38 @@ namespace WpfAppLaba1
 
         private void ButtonOperation_Click(object sender, RoutedEventArgs e)
         {
+            x = Convert.ToDouble(textBox.Text);
+            operation = (sender as Button).Content.ToString()[0];
+            textBox.Text = "0";
+        }
 
+        private void ButtonEnter_Click(object sender, RoutedEventArgs e)
+        {
+            double y = Convert.ToDouble(textBox.Text);
+            double result = 0;
+            switch(operation)
+            {
+                case '+': result = x + y; break;
+                case '-': result = x - y; break;
+                case '*': result = x * y; break;
+                case '/': result = x / y; break;
+
+            }
+            textBox.Text = result.ToString();
+        }
+
+        private void ButtonPoint_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.Text.Length >= 17 | textBox.Text.Contains(','))
+                return;
+            textBox.Text += ",";
+        }
+
+        private void ButtonClear_Click(object sender, RoutedEventArgs e)
+        {
+            x = 0;
+            operation = '\0';
+            textBox.Text = "0";
         }
     }
 }
