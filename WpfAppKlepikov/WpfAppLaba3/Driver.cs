d302c741c60ed7e2987b866cac51f2fd1cebc5b2
EvgeniyKlepikov <e_klepikov@mail.ru>
2024-12-18 22:55:35 +0300

    Лабораторная работа 3.

--- WpfAppKlepikov/WpfAppLaba3/Driver.cs
+++ WpfAppKlepikov/WpfAppLaba3/Driver.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +11,7 @@ namespace WpfAppLaba3
     enum GENDER { male, female, variant };
     enum COLOREYES { brown, green, blue, gray, black };
 
-    public class Driver
+    public class Driver : INotifyPropertyChanged
     {
         int number;
         char class1;
@@ -28,19 +30,98 @@ namespace WpfAppLaba3
         {
         }
 
-        public int Number { get => number; set => number = value; }
-        public char Class1 { get => class1; set => class1 = value; }
-        public double Hgt { get => hgt; set => hgt = value; }
-        public string Name { get => name; set => name = value; }
-        public string Address { get => address; set => address = value; }
-        public DateTime Dob { get => dob; set => dob = value; }
-        public DateTime Iss { get => iss; set => iss = value; }
-        public DateTime Exp { get => exp; set => exp = value; }
-        public bool Donor { get => donor; set => donor = value; }
+        public int Number
+        {
+            get => number;
+            set
+            {
+                number = value;
+                OnPropertyChanged(nameof(number));
+            }
+        }
+        public char Class1
+        {
+            get => class1;
+            set
+            {
+                class1 = value;
+                OnPropertyChanged(nameof(class1));
+            }
+        }
+        public double Hgt 
+        { 
+            get => hgt; 
+            set 
+            {
+                hgt = value;
+                OnPropertyChanged(nameof(hgt));
+            } 
+        }
+        public string Name 
+        { 
+            get => name; 
+            set 
+            {
+                name = value;
+                OnPropertyChanged(nameof(name));
+            } 
+        }
+        public string Address 
+        { 
+            get => address; 
+            set 
+            {
+                address = value;
+                OnPropertyChanged(nameof(address));
+            } 
+        }
+        public DateTime Dob
+        {
+            get => dob;
+            set
+            {
+                dob = value;
+                OnPropertyChanged($"Dob");
+            }
+        }
+        public DateTime Iss
+        {
+            get => iss;
+            set
+            {
+                iss = value;
+                OnPropertyChanged($"{nameof(iss)}");
+            }
+        }
+        public DateTime Exp
+        {
+            get => exp;
+            set
+            {
+                exp = value;
+                OnPropertyChanged($"{nameof(exp)}");
+            }
+        }
+        public bool Donor 
+        { 
+            get => donor; 
+            set 
+            {
+                donor = value;
+                OnPropertyChanged(nameof(donor));
+            } 
+        }
         public string UriImage { get => uriImage; set => uriImage = value; }
         internal GENDER Gender { get => gender; set => gender = value; }
         internal COLOREYES Eyes { get => eyes; set => eyes = value; }
 
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
         public override string? ToString()
         {
             return $"N{Number} {Class1} from {Iss:d} to {Exp:d}.\n{Name}, {Gender} Dob({Dob:d}). {Address}. Height {Hgt:0}.\nEyes {Eyes}. " +
--- WpfAppKlepikov/WpfAppLaba3/MainWindow.xaml.cs
+++ WpfAppKlepikov/WpfAppLaba3/MainWindow.xaml.cs
@@ -24,44 +24,46 @@ namespace WpfAppLaba3
         public MainWindow()
         {
             InitializeComponent();
-            datePickerDOB.SelectedDate = DateTime.Now;
-            datePickerISS.SelectedDate = DateTime.Now;
-            datePickerEXP.SelectedDate = DateTime.Now;
-            foreach (COLOREYES color in Enum.GetValues(typeof(COLOREYES)))
-            {
-                comboBox.Items.Add(color);
-            }
-            comboBox.SelectedIndex = 0;
+            //datePickerDOB.SelectedDate = DateTime.Now;
+            //datePickerISS.SelectedDate = DateTime.Now;
+            //datePickerEXP.SelectedDate = DateTime.Now;
+            //foreach (COLOREYES color in Enum.GetValues(typeof(COLOREYES)))
+            //{
+            //    comboBox.Items.Add(color);
+            //}
+            //comboBox.SelectedIndex = 0;
+            newDriver();
+            grid.DataContext = driver;
         }
 
         private void ButtonSave_Click(object sender, RoutedEventArgs e)
         {
-            driver.Name = textBoxName.Text;
-            int temp;
-            Int32.TryParse(textBoxNumber.Text, out temp);
-            driver.Number = temp;
-            driver.Address = textBoxAddress.Text;
-            if (textBoxClass.Text.Length > 0)
-            {
-                driver.Class1 = textBoxClass.Text[0];
-            }
-            else
-            {
-                driver.Class1 = 'A';
-            }
-            //driver.Eyes = comboBox.SelectedValuePath;
-            driver.Dob = (DateTime)datePickerDOB.SelectedDate;
-            driver.Iss = (DateTime)datePickerISS.SelectedDate;
-            driver.Exp = (DateTime)datePickerEXP.SelectedDate;
-            if (radioButtonMale.IsChecked == true) { driver.Gender = GENDER.male; }
-            if (radioButtonFemale.IsChecked == true) { driver.Gender = GENDER.female; }
-            if (radioButtonVariant.IsChecked == true) { driver.Gender = GENDER.variant; }
-            driver.Donor = checkBoxDonor.IsChecked == true;
-            driver.Hgt = slider.Value;
+            //driver.Name = textBoxName.Text;
+            //int temp;
+            //Int32.TryParse(textBoxNumber.Text, out temp);
+            //driver.Number = temp;
+            //driver.Address = textBoxAddress.Text;
+            //if (textBoxClass.Text.Length > 0)
+            //{
+            //    driver.Class1 = textBoxClass.Text[0];
+            //}
+            //else
+            //{
+            //    driver.Class1 = 'A';
+            //}
+            ////driver.Eyes = comboBox.SelectedValuePath;
+            //driver.Dob = (DateTime)datePickerDOB.SelectedDate;
+            //driver.Iss = (DateTime)datePickerISS.SelectedDate;
+            //driver.Exp = (DateTime)datePickerEXP.SelectedDate;
+            //if (radioButtonMale.IsChecked == true) { driver.Gender = GENDER.male; }
+            //if (radioButtonFemale.IsChecked == true) { driver.Gender = GENDER.female; }
+            //if (radioButtonVariant.IsChecked == true) { driver.Gender = GENDER.variant; }
+            //driver.Donor = checkBoxDonor.IsChecked == true;
+            //driver.Hgt = slider.Value;
             MessageBox.Show(driver.ToString());
         }
 
-        private void ButtonLoad_Click(object sender, RoutedEventArgs e)
+        void newDriver()
         {
             driver.Name = "Severus Snape";
             driver.Class1 = 'A';
@@ -75,23 +77,41 @@ namespace WpfAppLaba3
             driver.Exp = new DateTime(2038, 10, 22);
             driver.Donor = true;
             driver.UriImage = "Images/man.jpg";
+        }
 
-            textBoxName.Text = driver.Name;
-            textBoxClass.Text = driver.Class1.ToString();
-            textBoxAddress.Text = driver.Address;
-            textBoxNumber.Text = driver.Number.ToString();
-            slider.Value = driver.Hgt;
-            if (driver.Gender == GENDER.male) radioButtonMale.IsChecked = true;
-            if (driver.Gender == GENDER.female) radioButtonFemale.IsChecked = true;
-            if (driver.Gender == GENDER.variant) radioButtonVariant.IsChecked = true;
-            //comboBox.SelectedValue = driver.Eyes;
-            comboBox.SelectedItem = driver.Eyes;
-            datePickerDOB.SelectedDate = driver.Dob;
-            datePickerISS.SelectedDate = driver.Iss;
-            datePickerEXP.SelectedDate = driver.Exp;
-            checkBoxDonor.IsChecked = driver.Donor;
-            image.Source = new BitmapImage(new Uri(driver.UriImage, UriKind.RelativeOrAbsolute));
+        private void ButtonLoad_Click(object sender, RoutedEventArgs e)
+        {
+
+            //textBoxName.Text = driver.Name;
+            //textBoxClass.Text = driver.Class1.ToString();
+            //textBoxAddress.Text = driver.Address;
+            //textBoxNumber.Text = driver.Number.ToString();
+            //slider.Value = driver.Hgt;
+            //if (driver.Gender == GENDER.male) radioButtonMale.IsChecked = true;
+            //if (driver.Gender == GENDER.female) radioButtonFemale.IsChecked = true;
+            //if (driver.Gender == GENDER.variant) radioButtonVariant.IsChecked = true;
+            ////comboBox.SelectedValue = driver.Eyes;
+            //comboBox.SelectedItem = driver.Eyes;
+            //datePickerDOB.SelectedDate = driver.Dob;
+            //datePickerISS.SelectedDate = driver.Iss;
+            //datePickerEXP.SelectedDate = driver.Exp;
+            //checkBoxDonor.IsChecked = driver.Donor;
+            //image.Source = new BitmapImage(new Uri(driver.UriImage, UriKind.RelativeOrAbsolute));
             //MessageBox.Show(driver.ToString());
+
+            driver.Name = "Морфеус";
+            driver.Address = "Зион";
+            driver.Number = 888;
+            driver.Donor = false;
+            driver.Class1 = 'B';
+            driver.Hgt = 250;
+            driver.Iss = new DateTime(2018, 5, 11);
+            driver.Gender = GENDER.variant;
+            driver.Eyes = COLOREYES.brown;
+            driver.UriImage = "Images/man.jpg";
+            driver.Dob = new DateTime(1968, 5, 1);
+            driver.Exp = new DateTime(2038, 10, 22);
+
         }
 
         private void Image_MouseDown(object sender, MouseButtonEventArgs e)
