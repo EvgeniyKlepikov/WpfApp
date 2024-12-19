7de001bd91e3771cccce8369137006683c096d21
EvgeniyKlepikov <e_klepikov@mail.ru>
2024-12-19 16:57:53 +0300

    Лабораторная работа 4.

---
+++ WpfAppKlepikov/WpfAppLaba4/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppLaba4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Canvas[] canvases = new Canvas[3];
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.ResizeMode = ResizeMode.NoResize;
+            for (int i = 0; i < canvases.Length; i++)
+            {
+                canvases[i] = new Canvas();
+                Grid.SetRow(canvases[i], i);
+                grid.Children.Add(canvases[i]);
+            }
+        }
+    }
+}
---
+++ WpfAppKlepikov/WpfAppLaba4Hippodrome/Horse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppLaba4Hippodrome
+{
+    class Horse
+    {
+        int speed = 0;
+        int position = 0;
+        float x = 0;
+        bool isFinish = false;
+
+        public Horse(int speed)
+        {
+            this.Speed = speed;
+        }
+
+        public int Speed { get => speed; set => speed = value; }
+        public int Position { get => position; set => position = value; }
+        public float X { get => x; set => x = value; }
+        public bool IsFinish { get => isFinish; set => isFinish = value; }
+    }
+}
