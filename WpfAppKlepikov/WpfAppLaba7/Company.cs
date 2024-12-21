5ff472d19618a3ee3da26edc0b0ce4f72313b981
EvgeniyKlepikov <e_klepikov@mail.ru>
2024-12-21 22:05:23 +0300

    Лабораторная работа 7.

---
+++ WpfAppKlepikov/WpfAppLaba7/Company.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppLaba7
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Headquarters { get; set; }
+        public DateTime DateEstablishment { get; set; }
+        public Company() { }
+        public Company(int id, string name, string? headquarters, DateTime dateEstablishment)
+        {
+            Id = id;
+            Name = name;
+            Headquarters = headquarters;
+            DateEstablishment = dateEstablishment;
+        }
+
+        public override string? ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Headquarters: {Headquarters}, DateEstablishment: {DateEstablishment:D}";
+        }
+    }
+}
---
+++ WpfAppKlepikov/WpfAppLaba7/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppLaba7
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateBirth { get; set; }
+        public Company Job { get; set; }
+        public decimal Salary { get; set; }
+        public Person()
+        {
+            DateBirth = DateTime.MinValue;
+        }
+        public Person(int id, string name, DateTime dateBirth, Company job, decimal salary)
+        {
+            ID = id;
+            Name = name;
+            DateBirth = dateBirth;
+            Job = job;
+            Salary = salary;
+        }
+        public override string? ToString()
+        {
+            return $"ID: {ID}, Name: {Name}, Date of birth: {DateBirth:D}, Company: {Job?.Name}, Salary: {Salary:C}";
+        }
+    }
+}
