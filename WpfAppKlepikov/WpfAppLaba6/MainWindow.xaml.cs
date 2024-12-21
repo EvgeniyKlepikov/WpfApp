0f4b184ceb3919864ed21df4cc7fffe2722695de
EvgeniyKlepikov <e_klepikov@mail.ru>
2024-12-21 20:08:39 +0300

    Лабораторная работа 6.

--- WpfAppKlepikov/WpfAppLaba6/MainWindow.xaml.cs
+++ WpfAppKlepikov/WpfAppLaba6/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfAppLaba6
 {
@@ -17,9 +19,11 @@ namespace WpfAppLaba6
     public partial class MainWindow : Window
     {
         Intergral intergral;
+        BackgroundWorker backgroundWorker;
         public MainWindow()
         {
             InitializeComponent();
+            backgroundWorker = (BackgroundWorker)this.Resources["worker"];
         }
 
         private void buttonParams_Click(object sender, RoutedEventArgs e)
@@ -32,12 +36,16 @@ namespace WpfAppLaba6
 
         private void buttonD_Click(object sender, RoutedEventArgs e)
         {
-            Calculate();
+            if (intergral == null) return;
+            Thread thread = new Thread(Calculate);
+            thread.Start();
         }
 
         private void buttonW_Click(object sender, RoutedEventArgs e)
         {
-
+            buttonD.IsEnabled = false;
+            buttonW.IsEnabled = false;
+            backgroundWorker.RunWorkerAsync();
         }
 
         private void buttonA_Click(object sender, RoutedEventArgs e)
@@ -49,13 +57,63 @@ namespace WpfAppLaba6
             if (intergral == null) return;
             int n = intergral.N;
             double h = (intergral.B - intergral.A) / n;
+            var step = Math.Round((double)(n) / 100);
             double S = 0;
             for (int i = 0; i <= n; i++)
             {
                 double x = intergral.A + h * i;
                 S += intergral.func(x) * h;
+                if (i % step == 0)
+                {
+                    Dispatcher.BeginInvoke(DispatcherPriority.Normal,
+                        new Action(() => pBar.Value = i / step));
+                }
+                Dispatcher.BeginInvoke
+                    (
+                        DispatcherPriority.Normal,
+                        () => listBox.Items.Add(String.Format($"x = {x:0.00} S = {S:0.00000}"))
+                    );
+                Thread.Sleep(20);
             }
             MessageBox.Show($"S = {S:0.00000}");
+        }
+
+        private void BackgroundWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            if (intergral == null) return;
+            int n = intergral.N;
+            double h = (intergral.B - intergral.A) / n;
+            var step = Math.Round((double)(n) / 100);
+            double S = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                double x = intergral.A + h * i;
+                S += intergral.func(x) * h;
+                if (i % step == 0)
+                {
+                    if (backgroundWorker != null && backgroundWorker.WorkerReportsProgress)
+                        backgroundWorker.ReportProgress((int)(i / step));
+                }
+                Dispatcher.BeginInvoke
+                    (
+                        DispatcherPriority.Normal,
+                        () => listBox.Items.Add(String.Format($"x = {x:0.00} S = {S:0.00000}"))
+                    );
+                Thread.Sleep(20);
+            }
+            MessageBox.Show($"S = {S:0.00000}");
+
+        }
+
+        private void BackgroundWorker_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            pBar.Value = e.ProgressPercentage;
+        }
+
+        private void BackgroundWorker_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            buttonD.IsEnabled = true;
+            buttonW.IsEnabled = true;
         }
     }
 }
