1f71c8a36015625930acd6623284b221c734eee2
EvgeniyKlepikov <e_klepikov@mail.ru>
2024-12-22 21:19:31 +0300

    Лабораторная работа 8.

--- WpfAppKlepikov/WpfAppLaba8/MainWindow.xaml.cs
+++ WpfAppKlepikov/WpfAppLaba8/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfAppLaba8.Models;
 
 namespace WpfAppLaba8
 {
@@ -20,9 +22,41 @@ namespace WpfAppLaba8
     /// </summary>
     public partial class MainWindow : Window
     {
+        EntityContext entityContext;
         public MainWindow()
         {
             InitializeComponent();
+            entityContext = new EntityContext();
+            entityContext.Students.Load();
+            dGrid.ItemsSource = entityContext.Students.Local.ToBindingList();
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (dGrid.SelectedItems.Count == 0) return;
+            for (int i = dGrid.SelectedItems.Count - 1; i >= 0; i--)
+            {
+                Student student = dGrid.SelectedItems[i] as Student;
+                if(student != null)
+                    entityContext.Students.Remove(student);
+            }
+            entityContext.SaveChanges();
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            entityContext.SaveChanges();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            entityContext.Dispose();
         }
     }
 }
---
+++ WpfAppKlepikov/WpfAppLaba8/Models/EntityContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppLaba8.Models
+{
+    public class EntityContext : DbContext
+    {
+        public EntityContext() : base("DefaultConnection")
+        {
+        }
+        public DbSet<Student> Students { get; set; }    
+    }
+}
