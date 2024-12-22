1374f99da52fe76b69f45297cc3fd943285b1416
EvgeniyKlepikov <e_klepikov@mail.ru>
2024-12-22 16:47:48 +0300

    Лабораторная работа 7.

--- WpfAppKlepikov/WpfAppLaba7Adapter/MainWindow.xaml.cs
+++ WpfAppKlepikov/WpfAppLaba7Adapter/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,33 @@ namespace WpfAppLaba7Adapter
     /// </summary>
     public partial class MainWindow : Window
     {
+        DataTable personTable = new DataTable();
         public MainWindow()
         {
             InitializeComponent();
+            Fill();
+        }
+        private void Fill()
+        {
+            personTable.Rows.Clear();
+            personTable = Person.ViewAll();
+            personGrid.ItemsSource = personTable.DefaultView;
+        }
+
+        private void ButtonView_Click(object sender, RoutedEventArgs e)
+        {
+            Fill();
+        }
+
+        private void ButtonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            Person.Update();
+            Fill();
+        }
+
+        private void ButtonFind_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
